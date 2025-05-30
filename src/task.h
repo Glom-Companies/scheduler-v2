3cb2d7bd4101a6a869f1d14aa10094dadb2008a4
Glom-Companies <stoniot005@gmail.com>
2025-05-30 12:19:52 +0100

    Update of the Structure of an task Deletion of .o file

--- src/task.h
+++ src/task.h
@@ -1,28 +1,40 @@
 #ifndef TASK_H
 #define TASK_H
 
-//States of a task
+#include <sys/types.h> // pour pid_t
+
+//Types de tâches
 
 typedef enum {
-    NEW,
+    TASK_CONV_VIDEO = 0,
+    TASK_COMPRESS = 1,
+    TASK_UPDATE = 2,
+    TASK_CLONE = 3
+} task_type_t;
+
+typedef enum {
     READY,
     RUNNING,
-    WAITING,
     TERMINATED
-} state_t;
+} task_state_t;
 
-//Structure that describes a task
+//Structure de description d'une tâche
 typedef struct Task {
-    int pid;
-    int priority;
-    char description[256];
-    state_t state;
-    struct Task *next; //To chain tasks in a queue
+    int pid; //pid du fils
+    int priority; //priorité pour ordonnancement PRIORITY
+    task_type_t type; //quel type de tâche (conversion, compression)
+    char *param1; //paramètre 1: chemin ou URL
+    char *param2; //param2 : chemin de sortie du dossier
+    struct Task *next; //pour enchainer dan la file
 } Task;
 
-//Functions prototyoes on tasks
-Task* create_task(int priority, const char *desc);
+//Créer une tâche (les chaînes p1 et p2 sont dupliquées)
+Task* create_task(task_type_t type, int priority, const char *p1, const char *p2);
+
+//Libérer une tâche
 void free_task(Task *t);
+
+//Afficher la tâche (pour print_queue)
 void print_task(const Task *t);
 
 #endif // TASK_H
