6cb145dc8d66dfb7ddc1d326db6377a3a53818c7
Glom-Companies <stoniot005@gmail.com>
2025-05-30 12:38:36 +0100

    Task.c functions update (conv, compress, update, gitclone)

--- src/task.c
+++ src/task.c
@@ -1,41 +1,66 @@
+#include "task.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "task.h"
+
 
 //creer une nouvelle tâche en mémoire
-Task* create_task(int priority, const char *desc) {
+Task *create_task(task_type_t type, int priority, const char *p1, const char *p2) {
+
     Task *t = (Task*)malloc(sizeof(Task));
-    if (!t){
-        perror("Erreur malloc create_task");
-        exit(EXIT_FAILURE);
-    }
+    if (!t) returb NULL;
+
     t->pid = -1; //sera fixé après fork
     t->priority = priority;
-    strncpy(t->description, desc, 255);
-    t->description[255] = '\0';
-    t->state = NEW;
+    t->type = type;
+    t->state = READY;
+
+    if (p1) {
+        t->param1 = strdup(p1);
+        if (!t->param1) {
+            free(t);
+            return NULL;
+        }
+    } else {
+        t->param1 = NULL;
+    }
+
+    if (p2) {
+        t->param2 = strdup(p2);
+        if (!t->param2) {
+            if (t->param1) free(t->param1);
+            free(t);
+            return NULL;
+        }
+    } else {
+        t->param2 = NULL;
+    }
+
     t->next = NULL;
     return t;
 }
 
 //Libère la structure d'une tâche
 void free_task(Task *t) {
-    if (t) free(t);
+    if (!t) return;
+    if (t->param1) free(t->param1);
+    if (t->param2) free(t->param2);
+    free(t);
 }
 
 //Affiche une tâche (pour debug).
 void print_task(const Task *t) {
     if (!t) return;
-    const char *state_str;
-    switch (t->state) {
-        case NEW:        state_str = "NEW"; break;
-        case READY:      state_str = "READY"; break;
-        case RUNNING:    state_str = "RUNNING"; break;
-        case WAITING:    state_str = "WAITING"; break;
-        case TERMINATED: state_str = "TERMINATED"; break;
-        default:         state_str = "UNKNOWN"; break;
+    //Affichage selon le type
+    const char *type_str = "";
+    switch (t->type) {
+        case TASK_CONV_VIDEO: type_str = "Conversion"; break;
+        case TASK_COMPRESS: type_str = "Compression"; break;
+        case TASK_UPDATE: type_str = "MiseAJour"; break;
+        case TASK_CLONE: type_str = "ClonageGit"; break;
+        default: type_str = "Inconnu"; break;
+        
     }
-    printf("Task: PID=%d | Priorité=%d | Etat=%s | Description=\"%s\"\n",
-           t->pid, t->priority, state_str, t->description);
+    printf("Task: PID=%d | Type=%s | Etat=%s | Priorité=%d | État=%d\n",
+           t->pid, type_str, t->priority, t->state);
 }
--- src/task.h
+++ src/task.h
@@ -25,11 +25,12 @@ typedef struct Task {
     task_type_t type; //quel type de tâche (conversion, compression)
     char *param1; //paramètre 1: chemin ou URL
     char *param2; //param2 : chemin de sortie du dossier
+    task_state_t state; //etat de la tâche
     struct Task *next; //pour enchainer dan la file
 } Task;
 
 //Créer une tâche (les chaînes p1 et p2 sont dupliquées)
-Task* create_task(task_type_t type, int priority, const char *p1, const char *p2);
+Task *create_task(task_type_t type, int priority, const char *p1, const char *p2);
 
 //Libérer une tâche
 void free_task(Task *t);
